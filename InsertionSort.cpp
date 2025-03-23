68650b4ed79cba79ceef6ee6863bfb4c38fdc5b2
FahreziP <fahrezipurwadianitra@gmail.com>
2025-03-23 15:16:27 +0700

    menambahakn void input yang berisi while, if...else, for

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -4,3 +4,30 @@ using namespace std;
 int arr[20]; // Membuat Array dengan panjang data 20
 int n; // Membuat Variable inputan n
 
+void input()
+{ // Prosedur Input
+    while (true)
+    {
+        cout << "Masukkan Jumlah data pada Array : "; // Membuat Inputan Jumlah element Array
+        cin >> n; // Memanggil Variable inputan n
+
+        if (n <= 20)
+        {
+        break;
+        }
+        else
+        {
+        cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n"; // Menampilkan pesan jika data lebih dari 20
+        }
+    }
+    cout << endl; // Membuat jarak per baris program
+    cout << "======================" << endl; // Tampilan untuk susunan data element Array
+    cout << "Masukkan Element Array" << endl;
+    cout << "======================" << endl;
+
+    for (int i = 0; i < n; i++) // Menggunakan perulangan atau menginputkan nilai data n
+    {
+    cout << "Data ke-" << (i + 1) << ": "; // Memasukkan atau Meninputkan nilai data n
+    cin >> arr[i]; // Menyimpan nilai data n kedalam Array arr
+    }
+}
